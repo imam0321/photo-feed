2acdecbf994a84dc8aef61a372393410f5fd0ac0
imam0321 <imam.hossain0321@gmail.com>
2025-02-13 02:46:41 +0600

    feat: add photo details page and enhance image fetching with dynamic API integration

--- app/[lang]/page.js
+++ app/[lang]/page.js
@@ -1,9 +1,7 @@
 import { PhotoList } from "@/components/PhotoList/PhotoList";
 import { getDictionary } from "./dictionaries";
 
-export default async function Home({ params: { lang } }) {
-  const dictionary = await getDictionary(lang);
-
+export default async function Home() {
   const response = await fetch(`${process.env.BASE_API_URL}/photos`);
   const photos = await response.json();
 
